cf0d9a57b4732acd75cec52127540249555d717b
Ben Wells <mrbrwells@gmail.com>
2014-04-26 15:20:02 -0400

    updated secret token in prep for production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,9 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Benwellsmusic::Application.config.secret_key_base = '03fc5aafe59263ca7e250e3bb896691a562a7d8e06d9bb9ddfac8502e8d10f4d70681eb3132abd9749f37a9edf2a59b67d3cbeef1a57894a8a8a218d48be5708'
+# Benwellsmusic::Application.config.secret_key_base = '03fc5aafe59263ca7e250e3bb896691a562a7d8e06d9bb9ddfac8502e8d10f4d70681eb3132abd9749f37a9edf2a59b67d3cbeef1a57894a8a8a218d48be5708'
+Benwellsmusic::Application.config.secret_key_base = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
