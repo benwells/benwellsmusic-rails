51548e2eefe7c90dbe9c3bf97ee0a717aad5f533
Ben Wells <mrbrwells@gmail.com>
2014-04-26 09:54:50 -0400

    first commmit with static pages controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Benwellsmusic::Application.config.secret_key_base = '03fc5aafe59263ca7e250e3bb896691a562a7d8e06d9bb9ddfac8502e8d10f4d70681eb3132abd9749f37a9edf2a59b67d3cbeef1a57894a8a8a218d48be5708'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Benwellsmusic::Application.config.session_store :cookie_store, key: '_benwellsmusic_session'
